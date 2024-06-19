6e08847c0bb0dcd178c6e36d8636a54060b70275
Marshall Jones <marshall@vgs.io>
2024-06-19 11:38:26 -0700

    Missed files
    Signed-off-by: Marshall Jones <marshall@vgs.io>

--- iac/logs/test-delete-vgs.sh
+++ iac/logs/test-delete-vgs.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-export ORGANIZATION_ID=ACoKRUTV7ETUsqWjMJGEWJZb
+export ORGANIZATION_ID=$(echo ACoKRUTV7ETUsqWjMJGEWJZb | tr '[:upper:]' '[:lower:]')
 export AWS_PROFILE=dev/logs/sso
 export AWS_REGION=us-east-1
 export CUSTOMER_AWS_ACCOUNT_ID=767398127962
